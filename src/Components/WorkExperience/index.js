dace0f7ca0bfd745100810a38c4b1fb502d7a6e4
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-22 00:35:53 -0800

    started making semi dummy information to fill in the work experience part.

--- src/Components/WorkExperience/index.js
+++ src/Components/WorkExperience/index.js
@@ -1,10 +1,24 @@
 import "./index.css";
 
+const wrkExp = [{
+    name: "Darbe",
+    joined: "November 2020",
+    left:"Present employer",
+    title: "Full Stack Developer",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus felis at urna feugiat rutrum. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi luctus neque quis sem pretium mollis. Nullam commodo sapien id purus volutpat, quis euismod quam."
+}, {
+    name: "Legacy Medical",
+    joined: "April 2021",
+    left:"june 2021",
+    title: "Medical Records Clerk",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum faucibus felis at urna feugiat rutrum. Aliquam erat volutpat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi luctus neque quis sem pretium mollis. Nullam commodo sapien id purus volutpat, quis euismod quam."
+}]
+
 function WorkExperience() {
 
     return(
         <div className="container-exp">
-            
+
         </div>
     )
 }
