cdcfea563638beae175f029e9b7a71d37f8b5263
Drkeck <k3ckl3y.a@gmail.com>
2022-01-22 04:51:59 -0800

    filled in some sections of that tech stack

--- src/Components/TechKnowledge/index.js
+++ src/Components/TechKnowledge/index.js
@@ -1,11 +1,23 @@
 import "./index.css";
 
-function TechSkills() {
+const tek = ['React.js', 'MongoDB', 'Express.js', 'node.js', 'Axios', 'Next.js', 'Redux', 'git', 'jQuery', 'Materialize', 'Socket.io', 'Moment.js', 'React native', 'Electron.js'];
+const lang = ['HTML', 'CSS', 'JavaScript', 'SQL'];
 
+function TechSkills() {
+    let i = 0
     return(
         <div>
             <h2>Tech Skills</h2>
-            
+            <div className="tek-con">
+                {tek.map(tek => {
+                    i++
+                return(
+                    <div key={i}>
+                        <p>{tek}</p>
+                    </div>
+                    );
+                })}
+            </div>
         </div>
     )
 }
