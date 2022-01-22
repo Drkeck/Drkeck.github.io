cd9aa8d981310b74a833655c665e9e4224a0df94
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-21 23:56:32 -0800

    currently building out the Bio section with placeholder image and text so i can style this thing and then build my own image and style around it.

---
+++ src/Components/Bio/index.js
@@ -0,0 +1,22 @@
+import react from "react";
+
+function Bio() {
+
+    return(
+        <div>
+            <img src="https://via.placeholder.com/150"></img>
+            <div>
+                <h1>Alexander keckley</h1>
+                <h3>Software Developer</h3>
+
+                <div>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus lacus massa, egestas ut porta sed, malesuada et ex. Sed volutpat molestie odio, ut ullamcorper neque luctus nec. Nunc ut porttitor ante. Etiam sollicitudin enim turpis, sit amet consequat ex ullamcorper ut. Sed consequat eros quis felis mollis ornare. Integer tincidunt mollis dui, et ornare nulla dictum eu. Etiam eu convallis felis. In sit amet lectus lacus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id magna tortor.</p>
+                    <p>Curabitur vitae ipsum in mauris placerat elementum. In bibendum nisl ipsum, quis ullamcorper odio venenatis sed. Pellentesque id viverra nulla. Ut nec turpis et ligula tempor rutrum. Nullam et felis lacinia, congue metus quis, dictum massa. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Fusce efficitur efficitur auctor. Pellentesque venenatis non neque ac gravida. Etiam a turpis feugiat mi vulputate pharetra nec vitae eros. Donec sit amet viverra tortor.</p>
+                    <p>Morbi diam nunc, aliquet at eleifend vel, cursus et dolor. Fusce molestie in ipsum sit amet accumsan. Integer suscipit sem sit amet condimentum condimentum. Morbi at euismod risus. Pellentesque quis dolor egestas quam dignissim lobortis vel non mauris. Donec at felis vel ante imperdiet aliquam. Sed et tempor ex, ut finibus sapien. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam rutrum mi eget orci ultrices, non posuere urna pellentesque.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Bio
