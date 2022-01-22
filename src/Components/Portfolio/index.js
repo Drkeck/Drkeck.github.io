ac73c791621841b660b3c72e1310909990678d16
Drkeck <k3ckl3y.a@gmail.com>
2022-01-22 01:44:51 -0800

    filling out basic info into the data generation, then using map to display it all evenly across, will scale when needed

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,10 +1,65 @@
 import "./index.css";
 
+const prtWrk = [
+    {
+        name: "Thrallchives",
+        github: "",
+        deployed: "",
+        description: "",
+        tech: ""
+    }, 
+    {
+        name: "DeceptiveBrownie Epk",
+        github: "",
+        deployed: "",
+        description: "",
+        tech: ""
+    }, 
+    {
+        name: "D&D Character Generator",
+        github: "",
+        deployed: "",
+        description: "",
+        tech: ""
+    }, 
+    {
+        name: "Darbe",
+        github: "",
+        deployed: "",
+        description: "",
+        tech: ""
+    }, 
+    {
+        name: "",
+        github: "",
+        deployed: "",
+        description: "",
+        tech: ""
+    }
+]
+
 function Portfolio() {
+
+    let i = 0;
 
     return(
         <div>
+            {prtWrk.map(project => {
+                const {
+                    name,
+                    github,
+                    deployed,
+                    description,
+                    tech
+                } = project;
+                i++
 
+             return(
+                <div key={i}>
+                    <h1>{name}</h1>
+                </div>
+             )
+        })}
         </div>
     )
 }
