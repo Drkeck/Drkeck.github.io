29c0c93bae4f47fdebacc5fe357d24ed0c03d34b
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2022-01-22 18:32:03 -0800

    forgot to change the links in the footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -7,8 +7,8 @@ function Footer() {
             <a href="emailto:k3ckl3y.a@gmail.com">Email</a>
             <a href="https://github.com/drkeck">Github</a>
             <p className="fade">Made In React, with love.</p>
-            <a href="https://linkedin.com/in/alexanderkeckley/">LinkedIn</a>
-            <a href="https://google.com">Resume</a>
+            <a href="https://www.linkedin.com/in/alexkeckley/">LinkedIn</a>
+            <a href="https://docs.google.com/document/d/1NRn6R1lbw27lBMFy45Gl_A--UkJw7daKU0XOPzRPOhM/edit?usp=sharing">Resume</a>
         </footer>
     )
 }
