f61398dc0fa3ed32369c9b1011dd6146180d1053
Drkeck <k3ckl3y.a@gmail.com>
2022-02-06 00:26:20 -0800

    added some special spice to the background, should be completely indipendent particles that are random

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { Bio, Footer, Portfolio, TechSkills, WorkExperience } from './Components';
+import Particles from 'react-tsparticles';
 import './App.css';
 
 function App() {
+
+  const particlesInit = (main) => {
+    // console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    // console.log(container);
+  };
+
   return (
     <div className="App">
       <Bio />
@@ -11,6 +23,100 @@ function App() {
       <h2>Portfolio</h2>
       <Portfolio />
       <Footer />
+      <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        background: {
+          color: {
+            value: "#BEBEBE",
+            opacity: 0,
+            size: 0,
+          },
+        },
+        fullScreen: {
+          zIndex: -1,
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: false,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 100,
+              duration: 1,
+            },
+            connect: {
+              distance: 1200,
+              links: {
+                opacity: 0.5,
+              },
+              radius: 60,
+            }
+          },
+        },
+        particles: {
+          color: {
+            value: "#FFFFFF",
+          },
+          links: {
+            color: "#FFFFFF",
+            distance: 80,
+            enable: true,
+            opacity: 0.25,
+            width: 2,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "out",
+            random: true,
+            speed: 3,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: false,
+              area: 1800,
+            },
+            value: 300,
+          },
+          opacity: {
+            value: 0.75,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 3,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
     </div>
   );
 }
