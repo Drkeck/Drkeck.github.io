a09133e97b684512c8eb0c64c6f909ac093715fe
keck <k3ckl3y.a@gmail.com>
2024-02-18 18:14:59 -0500

    removed particles and draggable

--- src/Components/Teckpoints/index.js
+++ src/Components/Teckpoints/index.js
@@ -1,13 +1,11 @@
 import React from "react";
-import Draggable from "react-draggable";
 import '../TechKnowledge/index.css'
 
 export default function TeckPoints({point}) {
+
     return(
-        <Draggable>
             <div className="tek-text">
                 <p>{point}</p>
             </div>
-        </Draggable>
     )
 }
