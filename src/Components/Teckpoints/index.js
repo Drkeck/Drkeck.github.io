568cff45d396a4f12834468b96f3963b6593fd81
keck <k3ckl3y.a@gmail.com>
2024-02-18 19:30:23 -0500

    re-add draggable

--- src/Components/Teckpoints/index.js
+++ src/Components/Teckpoints/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Draggable from "react-draggable";
 import '../TechKnowledge/index.css'
 
 export default function TeckPoints({point}) {
 
     return(
+          <Draggable>
             <div className="tek-text">
                 <p>{point}</p>
             </div>
+          </Draggable>
     )
 }
